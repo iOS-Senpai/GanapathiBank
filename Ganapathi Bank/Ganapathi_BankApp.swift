8516ea8374b26217eff8775471cd3488248fa89c
Pradeep Kumar <mukku.kumar@telsuagcg.com>
2026-07-09 13:08:24 +0530

    Initial Commit

---
+++ Ganapathi Bank/Ganapathi_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ganapathi_BankApp.swift
+//  Ganapathi Bank
+//
+//  Created by xavient on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Ganapathi_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
